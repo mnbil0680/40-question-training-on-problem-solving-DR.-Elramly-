ff60212110f0ad45647f353cc1ce53ab7fb1c9a5
mnbil0680 <160195215+mnbil0680@users.noreply.github.com>
2024-05-13 08:23:05 +0300

    Add files via upload

---
+++ #8.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+	const int LENGTH = 21;
+	char message[LENGTH]; 
+	cout << "Enter a sentence on the line below." << endl;
+	cin >> message; // tricky
+	cout << message << endl;
+	//  (Please) go away.
+	
+}
