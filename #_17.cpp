88463a031b68b62d84f23e466961c84a524d83ba
mnbil0680 <160195215+mnbil0680@users.noreply.github.com>
2024-05-14 04:10:34 +0300

    Add files via upload

---
+++ #_17.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int n, k = 5;
+	n = (100 % k ? k + 1 : k - 1);
+	cout << "n = " << n << " k = " << k << endl;
+	// expresion? true : false
+	// print(n = 4 k = 5)
+}
---
+++ #_18.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int n;
+	float x = 3.8;
+	n = int(x); // n = 3
+	cout << "n = " << n << endl;
+	//print(n = 3)
+}
